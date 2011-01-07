ff81cb8ab3c129783c15b7292f8739f658ff130f
olivier.locci <olivier.locci@40007b59-adb2-11b8-ccd7-3cccdbec7aaa>
2011-01-07 14:59:16 +0000

    Panier

--- www/test/unit/PanierTest.php
+++ www/test/unit/PanierTest.php
@@ -6,14 +6,13 @@
 // pour inclure la bibliothèque "LIME"
 include(dirname(__FILE__).'/../bootstrap/unit.php');
 // pour pouvoir accéder à la base de données de l'application vitrine via les classes générées par Propel
-new sfDatabaseManager(ProjectConfiguration::getApplicationConfiguration('vitrine',
-'test', true));
+new sfDatabaseManager(ProjectConfiguration::getApplicationConfiguration('vitrine','test', true));
 // instancier un nouveau test
 $t = new lime_test(10, new lime_output_color()); // on s'apprête à faire 10 tests
 // commencer ici les tests sur vos objets métier
 $p = new Panier();
 $a = new Article();
 $t->diag('addArticle()');
-$t->isa_ok($p->addArticle($a), 'void','Panier() renvoie rien');
+$t->isa_ok($p->addNewArticle($a,1), 'void','Panier() renvoie rien');
 
 ?>
