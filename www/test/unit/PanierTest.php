920e8c177ff78b8c8ab7207f1591274bac7dd2b3
michael.lefevre38 <michael.lefevre38@40007b59-adb2-11b8-ccd7-3cccdbec7aaa>
2011-01-07 14:39:58 +0000

    test unit panier

---
+++ www/test/unit/PanierTest.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// pour inclure la bibliothèque "LIME"
+include(dirname(__FILE__).'/../bootstrap/unit.php');
+// pour pouvoir accéder à la base de données de l'application vitrine via les classes générées par Propel
+new sfDatabaseManager(ProjectConfiguration::getApplicationConfiguration('vitrine',
+'test', true));
+// instancier un nouveau test
+$t = new lime_test(10, new lime_output_color()); // on s'apprête à faire 10 tests
+// commencer ici les tests sur vos objets métier
+$p = new Panier();
+
+
+?>
